28f39092bd901c8c5cc3c8a71a9d2496255d7c51
Masoud <mgolchin1993@gmail.com>
2022-11-27 19:26:19 +0330

    add helper functions

---
+++ Helpers/helpers.ts
@@ -0,0 +1,9 @@
+import { Tag } from "../components/TagInput";
+
+export function checkIfTagAlreadyExists(item: Tag, selectedItems : Tag[]) {
+    return selectedItems.some((selectedItem : Tag) => item.label === selectedItem.label)
+}
+
+export function slugify(str : string) {
+    return str.trim().replaceAll(/ /g, "-").toLowerCase();
+}
