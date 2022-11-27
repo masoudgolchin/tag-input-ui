e456a7bd5e632f359d289e7087c6606cde8d63fc
Masoud <mgolchin1993@gmail.com>
2022-11-27 19:27:00 +0330

    add function: AddTag

--- components/BaseTextInput.tsx
+++ components/BaseTextInput.tsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { ChangeEvent } from "react";
 
-export default function BaseTextInput() {
+type BaseTextInputProps = {
+  input: string;
+  setInput: (text: string) => void;
+};
+export default function BaseTextInput({ input, setInput }: BaseTextInputProps) {
   return (
     <>
       <input
         placeholder="Add tags..."
         type="text"
-        // value={inputText}
-        // onChange={inputTextHandler}
+        value={input}
+        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+          setInput(e.target.value)
+        }
         // onKeyDown={handleKeyDown}
         // onBlur={handleBlur}
       />
--- components/TagInput.tsx
+++ components/TagInput.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import BaseTextInput from "./BaseTextInput";
 import List from "./List";
 import Tags from "./Tags";
+import { checkIfTagAlreadyExists } from "../Helpers/helpers";
 
 export type Tag = {
   id: string;
@@ -10,12 +11,24 @@ export type Tag = {
 
 export default function TagInput() {
   const [showList, setShowList] = useState<boolean>(true);
+  const [input, setInput] = useState<string>("");
   const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
   const [searchResult, setSearchResult] = useState<Tag[]>([]);
   const [highlightedIndex, setHighlightedIndex] = useState<number>(0);
+
+  function addTag(item: Tag) {
+    setShowList(false);
+    if (checkIfTagAlreadyExists(item, selectedTags)) {
+      console.log("exists");
+      return;
+    }
+    setSelectedTags((prev) => [...prev, { ...item }]);
+    setInput("");
+  }
+
   return (
     <div>
-      <BaseTextInput />
+      <BaseTextInput input={input} setInput={setInput} />
       {showList && (
         <List
           searchResult={searchResult}
